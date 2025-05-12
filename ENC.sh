b8ed116d93e87a6c7de8d94ef3b5c2882fb6411c
VPSHOZOO <lordhozoo891@gmail.com>
2025-05-12 13:18:44 +0700

    Create ENC.sh

---
+++ ENC.sh
@@ -0,0 +1,70 @@
+!/bin/bash
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+function loading_animation() {
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    
+    echo -n "Memproses "
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+# Meminta password
+echo -e " \033[31;1m █░░ █▀█ █▀█ █▀▄ █░█ █▀█ ▀█ █▀█ █▀█"
+echo -e " \033[37;1m █▄▄ █▄█ █▀▄ █▄▀ █▀█ █▄█ █▄ █▄█ █▄█ \033[31;1m V 1.0.1 "
+echo -e "\033[37;1m┌──────────────────────────────────────────────────────────────┐ $white"
+echo -e "│ $white AUTHOR   : LORDHOZOO                                        │"
+echo -e "│ $white DILIRIS  : 2025-10-10 MEI SABTU                             │"
+echo -e "│ $white YOUTUBE  : LORDHOZOO                                        │"
+echo -e "│ $white TIKTOK   : LORDHOZOO                                        │" 
+echo -e "│ $white STATUS   : $green ONLINE                                       $white   │ $white "
+echo -e "└──────────────────────────────────────────────────────────────┘ $white"
+echo -e "Masukkan Password untuk menjalankan tools: "
+read -s password
+echo
+
+# Cek password (contoh password "rahasia")
+if [ "$password" != "123456" ]; then
+    echo "Password salah! Tools tidak dijalankan."
+    exit 1
+fi
+
+# Proses yang membutuhkan loading
+(
+    # Simulasi proses yang memakan waktu
+    sleep 3
+) &
+
+loading_animation $!
+
+echo -e "\nTools berhasil dijalankan! 👰 Sukses"
+clear
+
+
+echo -e "============================================"
+echo -e "=  👰. ENC EXECUTOR LORDHOZOO 👰           ="
+echo -e "============================================"
+echo -e ""
+echo -e "============================================"
+echo -e "= [01]. ENC BASH TERKUAT 💃                ="
+echo -e "============================================"
+echo -e "==========================================="
+echo -e "= [02]. ENC BASH TERKUAT 👰                ="
+echo -e "============================================"
+echo -e "==========================================="
+echo -e "= [03]. ENC BASH TERKUAT EMOJI 👁️👄👁️     ="
+echo -e "==========================================="
